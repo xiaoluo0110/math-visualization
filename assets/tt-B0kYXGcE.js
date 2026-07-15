0b16a7ac885187b66b3d265608659249e679221f
Luobr <1211571826@qq.com>
2026-07-15 20:43:19 +0800

    fix:更新部分ui样式

---
+++ assets/tt-B0kYXGcE.js
@@ -0,0 +1,1 @@
+import{G as g}from"./index-AEQE8qH7.js";function n(r,i){let t=r;try{t=String(g.global.t(r))}catch{t=r}return i?t.replace(/\{(\w+)\}/g,(o,l)=>i[l]!=null?String(i[l]):`{${l}}`):t}export{n as t};
