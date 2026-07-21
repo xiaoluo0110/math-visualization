a1953ec8f619ec15ba51a869dd6325d18f3bba1b
Luobr <1211571826@qq.com>
2026-07-21 20:37:01 +0800

    fix:更新部分优化的功能

--- assets/tt-BG9UAqU8.js
+++
@@ -1,1 +0,0 @@
-import{V as g}from"./index-CV_yIXXG.js";function n(r,i){let t=r;try{t=String(g.global.t(r))}catch{t=r}return i?t.replace(/\{(\w+)\}/g,(o,l)=>i[l]!=null?String(i[l]):`{${l}}`):t}export{n as t};
---
+++ assets/tt-Cu83RMMI.js
@@ -0,0 +1,1 @@
+import{Z as g}from"./index-DrZuCvaI.js";function n(r,i){let t=r;try{t=String(g.global.t(r))}catch{t=r}return i?t.replace(/\{(\w+)\}/g,(o,l)=>i[l]!=null?String(i[l]):`{${l}}`):t}export{n as t};
